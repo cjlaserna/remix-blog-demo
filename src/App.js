734d09b1d8d32f86dd4b32acec41bd8e3c688f39
Catherine Laserna <49669196+cjlaserna@users.noreply.github.com>
2024-09-06 20:18:50 -0700

    change size

--- src/App.js
+++ src/App.js
@@ -9,30 +9,30 @@ import Navigation from "./content/Navigation";
 import SideQuests from "./pages/SideQuests";
 
 const App = () => {
-  return (
-    <Router>
-      <>
-        <div className="holy-grail-grid">
-          <header className="header">
-            <Navigation />
-          </header>
-          <Routes>
-            <Route path="/" element={<Profile />} />
-            <Route path="/experiences" element={<Experiences />} />
-            <Route path="/achievements" element={<Achievements />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/sidequests" element={<SideQuests />} />
-          </Routes>
-        </div>
-      </>
-      <Link to="/sidequests">
-        <button className="btn btn-primary text-white btn-circle text-[2rem] fixed bottom-10 right-10 z-20 hidden md:block md:hover:animate-pulse">
-          ?
-        </button>
-      </Link>
-    </Router>
-  );
+	return (
+		<Router>
+			<>
+				<div className="holy-grail-grid !mt-[3rem]">
+					<header className="header">
+						<Navigation />
+					</header>
+					<Routes>
+						<Route path="/" element={<Profile />} />
+						<Route path="/experiences" element={<Experiences />} />
+						<Route path="/achievements" element={<Achievements />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/projects" element={<Projects />} />
+						<Route path="/sidequests" element={<SideQuests />} />
+					</Routes>
+				</div>
+			</>
+			<Link to="/sidequests">
+				<button className="btn btn-primary text-white btn-circle text-[2rem] fixed bottom-10 right-10 z-20 hidden md:block md:hover:animate-pulse">
+					?
+				</button>
+			</Link>
+		</Router>
+	);
 };
 
 export default App;
